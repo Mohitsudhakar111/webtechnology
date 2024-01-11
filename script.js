cbce8728dbcbdcb8321028f8cf1e84c4c7001b5c
Mohitsudhakar111 <154774716+Mohitsudhakar111@users.noreply.github.com>
2024-01-11 15:18:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,74 @@
+let div1 = document.createElement("div");
+document.body.appendChild(div1);
+
+div1.setAttribute( "id", "div1" );
+div1.setAttribute( "style","background-color:red" );
+
+let para11 = document.createElement("p");
+para11.innerText = " div1 para1 ";
+let para12 = document.createElement("p");
+para12.innerText = " div1 para2 ";
+
+document.getElementById("div1").appendChild(para11);
+document.getElementById("div1").appendChild(para12);
+
+let div2 = document.createElement("div");
+document.body.appendChild(div2);
+
+div2.setAttribute( "id", "div2" );
+div2.setAttribute( "style","background-color:blue" );
+
+let para21 = document.createElement("p");
+para21.innerText = " div2 para1 ";
+let para22 = document.createElement("p");
+para22.innerText = " div2 para2 ";
+
+document.getElementById("div2").appendChild(para21);
+document.getElementById("div2").appendChild(para22);
+
+
+let div3 = document.createElement("div");
+document.body.appendChild(div3);
+
+div3.setAttribute( "id", "div3" );
+div3.setAttribute( "style","background-color:green" );
+
+let para31 = document.createElement("p");
+para31.innerText = " div3 para1 ";
+let para32 = document.createElement("p");
+para32.innerText = " div3 para2 ";
+
+document.getElementById("div3").appendChild(para31);
+document.getElementById("div3").appendChild(para32);
+
+
+let div4 = document.createElement("div");
+document.body.appendChild(div4);
+
+div4.setAttribute( "id", "div4" );
+div4.setAttribute( "style","background-color:yellow" );
+
+let para41 = document.createElement("p");
+para41.innerText = " div4 para1 ";
+let para42 = document.createElement("p");
+para42.innerText = " div4 para2 ";
+
+document.getElementById("div4").appendChild(para41);
+document.getElementById("div4").appendChild(para42);
+
+
+let div5 = document.createElement("div");
+document.body.appendChild(div5);
+
+div5.setAttribute( "id", "div5" );
+div5.setAttribute( "style","background-color:pink" );
+
+let para51 = document.createElement("p");
+para51.innerText = " div5 para1 ";
+let para52 = document.createElement("p");
+para52.innerText = " div5 para2 ";
+
+document.getElementById("div5").appendChild(para51);
+document.getElementById("div5").appendChild(para52);
+
+
